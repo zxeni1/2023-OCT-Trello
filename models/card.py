9fc546ba31fe0705c70b9f3c53921913cd83799a
zxeni1 <zacxfitness@gmail.com>
2024-02-16 21:21:52 +1000

    ordered in card schema and get all cards in card controller

--- models/card.py
+++ models/card.py
@@ -1,4 +1,4 @@
-from init import db, ma 
+from init import db, ma
 from marshmallow import fields
 
 class Card(db.Model):
@@ -6,24 +6,32 @@ class Card(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
-    date = db.Column(db.Date) # Date the card was created
     description = db.Column(db.Text)
+    date = db.Column(db.Date) # Date the card was created
     status = db.Column(db.String)
     priority = db.Column(db.String)
 
-
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    user = db.relationship('User', back_populates="cards")
+    user = db.relationship('User', back_populates='cards')
 
-    # {id: 1 title: Card 1, user_id: 2}
+    # {id: 1, title: Card 1, user_id: 2}
+    # {
+    #   id: 1,
+    #   title: Card 1,
+    #   user: {
+    #       name: User 1,
+    #       email: user1@email.com
+    #   }
+    # }
 
-class CardSchema(ma.Schema): 
+class CardSchema(ma.Schema):
 
     user = fields.Nested('UserSchema', only = ['name', 'email'])
 
     class Meta:
         fields = ('id', 'title', 'description', 'date', 'status', 'priority', 'user')
+        ordered = True
 
 card_schema = CardSchema()
-card_schema = CardSchema(many=True)
+cards_schema = CardSchema(many=True)
