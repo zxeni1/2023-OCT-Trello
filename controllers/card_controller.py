ec696f6aa80a7fa427c750cdf03f932abbd46101
zxeni1 <zacxfitness@gmail.com>
2024-03-02 09:24:18 +1000

    Create Authorise_as_admin decorator function

--- controllers/card_controller.py
+++ controllers/card_controller.py
@@ -1,4 +1,5 @@
 from datetime import date
+import functools
 
 from flask import Blueprint, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
@@ -10,6 +11,23 @@ from controllers.comment_controller import comments_bp
 
 cards_bp = Blueprint('cards', __name__, url_prefix='/cards')
 cards_bp.register_blueprint(comments_bp)
+
+def authorise_as_admin(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        user_id = get_jwt_identity()
+        stmt = db.select(User).filter_by(id=user_id)
+        user = db.session.scalar(stmt)
+        # if the user is an admin
+        if user.is_admin:
+            # we will continue and run the decorated function
+            return fn(*args, **kwargs)
+        # else (if the user is NOT an admin)
+        else:
+            # return an error
+            return {"error": "Not authorised to delete a card"}, 403
+        
+    return wrapper
 
 # http://localhost:8080/cards - GET
 @cards_bp.route('/')
@@ -53,6 +71,7 @@ def create_card():
 # https://localhost:8080/cards/6 - DELETE
 @cards_bp.route('/<int:card_id>', methods=["DELETE"])
 @jwt_required()
+@authorise_as_admin
 def delete_card(card_id):
     # check user's admin status
     is_admin = is_user_admin()
@@ -85,7 +104,7 @@ def update_card(card_id):
     # if card exists
     if card:
         if str(card.user_id) != get_jwt_identity():
-            return {"error": "Only the owner can edit the cardeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwODczNzgyMSwianRpIjoiNjYzYjJlZjEtNGM2Ni00MjVmLThkYTQtOTViOTg0NDYxM2NkIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjIiLCJuYmYiOjE3MDg3Mzc4MjEsImNzcmYiOiI2NWViNzkxOC0xNTU3LTQ3NzItYmJiNS1kMTlhNGQ5NWIzYWMiLCJleHAiOjE3MDg4MjQyMjF9.ncQ0o17vGDrhBm0ohJ3POmo05XyJqktySlTvrBQITZg"}, 403
+            return {"error": "Only the owner can edit the card"}, 403
         # update the fields
         card.title = body_data.get('title') or card.title
         card.description = body_data.get('description') or card.description
@@ -100,9 +119,12 @@ def update_card(card_id):
         # return error msg
         return {'error': f'Card with id {card_id} not found'}, 404
     
-
+# this function as been replaced by auth admin decorator
 def is_user_admin():
     user_id = get_jwt_identity()
     stmt = db.select(User).filter_by(id=user_id)
     user = db.session.scalar(stmt)
     return user.is_admin
+
+
+
